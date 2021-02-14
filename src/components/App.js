e3f3f45d23bbf9d1b383e0d0667fbc1d53e4f446
ashwani436 <sharmagpl786@gmail.com>
2021-02-15 00:32:17 +0530

    save changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 import '../styles/App.css';
 import Selection from './Selection';
 import ColourSelector from './ColourSelector';
@@ -26,12 +26,12 @@ const title = 'Select the gradient and then the Box to change the color';
 const App = () => {
   let [nextBackground, selectNextBackground] = useState({ background: "" })
   const applyColor = (updateSelectionStyle) => {
-    updateSelectionStyle(nextBackground)
+    updateSelectionStyle({background:nextBackground.background})
   }
 
   return (
     <div id="master">
-      <h5 className="heading">{/* display title here */}</h5>
+      <h5 className="heading">{title}</h5>
 
       <div className="row">
         {colourConfig.map((config, index) => (
@@ -42,11 +42,11 @@ const App = () => {
       <div className='row' id="children-wrapper">
         {
           ["selection1", "selection2", "selection3"].map(key => (
-            <Selection key={key} applyColor={applyColor} />
+            <Selection key={key} applyColor={applyColor}/>
           ))
         }
       </div>
-    </div >
+    </div>
   )
 }
 
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,14 @@
+import React,{useState} from 'react';
+import '../styles/Child.css';
 
+const Selection = (props) => {
+	const [bg, selectbg] = useState({ background: "" })
+	const {applyColor,nextBackground} = props;
+	
+  return (
+  	<div className="fix-box" style={bg} onClick={()=>applyColor(selectbg)}>
+  		<h2 className="subheading">Selection</h2>
+  	</div>
+  )
+}
+export default Selection;
